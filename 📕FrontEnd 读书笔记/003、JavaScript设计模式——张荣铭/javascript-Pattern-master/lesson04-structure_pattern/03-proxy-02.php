b1cc373083edb9615d8dc9f46df953c7bfc83ea9
M_DoubleQ <122364450+MDoubleQ@users.noreply.github.com>
2023-05-18 13:09:43 +0800

    Design pattern

--- 📕FrontEnd 读书笔记/003、JavaScript设计模式——张荣铭/javascript-Pattern-master/lesson04-structure_pattern/03-proxy-02.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $proxy = $_POST["proxy"];
-    $callback = $_POST["callback"];
-    header("Location: ".$proxy."?callback=".$callback."&arg=success");
-?>
--- 📕FrontEnd 读书笔记/003、JavaScript设计模式——张荣铭/javascript-Pattern-master/lesson07-architecture_pattern/lib/event.js
+++
@@ -1,8 +0,0 @@
-F.module('lib/event',['lib/dom'],function(dom){
-    var events = {
-        on : function(id,type,fn){
-            dom.g(id)['on' + type] = fn;
-        }
-    };
-    return events;
-});
